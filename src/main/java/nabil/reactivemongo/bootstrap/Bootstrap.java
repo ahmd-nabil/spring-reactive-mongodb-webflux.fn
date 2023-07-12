a11ff0f35df1eebdc713e6d3e1bda0717f65d907
Ahmed Nabil <ahm3dnabil99@gmail.com>
2023-07-13 01:09:25 +0300

    add bootstrap class

---
+++ src/main/java/nabil/reactivemongo/bootstrap/Bootstrap.java
@@ -0,0 +1,76 @@
+package nabil.reactivemongo.bootstrap;
+
+import lombok.RequiredArgsConstructor;
+import nabil.reactivemongo.domain.Beer;
+import nabil.reactivemongo.domain.Customer;
+import nabil.reactivemongo.repositories.BeerRepository;
+import nabil.reactivemongo.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * @author Ahmed Nabil
+ */
+@Component
+@RequiredArgsConstructor
+public class Bootstrap implements CommandLineRunner {
+    private final BeerRepository beerRepository;
+    private final CustomerRepository customerRepository;
+    @Override
+    public void run(String... args) throws Exception {
+        beerRepository
+                .deleteAll()
+                .doOnSuccess(success-> populateBeers())
+                .subscribe();
+        customerRepository.deleteAll()
+                .doOnSuccess(success -> populateCustomers())
+                .subscribe();
+    }
+    private void populateBeers() {
+        Beer beer1 = Beer.builder()
+                .beerName("Galaxy Cat")
+                .beerStyle("Pale Ale")
+                .upc("12356")
+                .price(new BigDecimal("12.99"))
+                .quantityOnHand(122)
+                .createdDate(LocalDateTime.now())
+                .lastModifiedDate(LocalDateTime.now())
+                .build();
+
+        Beer beer2 = Beer.builder()
+                .beerName("Crank")
+                .beerStyle("Pale Ale")
+                .upc("12356222")
+                .price(new BigDecimal("11.99"))
+                .quantityOnHand(392)
+                .createdDate(LocalDateTime.now())
+                .lastModifiedDate(LocalDateTime.now())
+                .build();
+
+        Beer beer3 = Beer.builder()
+                .beerName("Sunshine City")
+                .beerStyle("IPA")
+                .upc("12356")
+                .price(new BigDecimal("13.99"))
+                .quantityOnHand(144)
+                .createdDate(LocalDateTime.now())
+                .lastModifiedDate(LocalDateTime.now())
+                .build();
+
+        beerRepository.save(beer1).subscribe();
+        beerRepository.save(beer2).subscribe();
+        beerRepository.save(beer3).subscribe();
+    }
+
+    private void populateCustomers() {
+        Customer customer1 = Customer.builder().firstName("Ahmed").lastName("Nabil").build();
+        Customer customer2 = Customer.builder().firstName("Mike").lastName("Ross").build();
+        Customer customer3 = Customer.builder().firstName("Phoebe").lastName("Buffay").build();
+        customerRepository.save(customer1).subscribe();
+        customerRepository.save(customer2).subscribe();
+        customerRepository.save(customer3).subscribe();
+    }
+}
